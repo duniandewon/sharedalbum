e11cac940bbed908688cc7539ca620d4ed1e7682
aboli <duniandewon@gmail.com>
2025-06-18 00:11:23 +0700

    refactor(useEventForm): use useReducer to handle complex state management and decide between edit and create mode

--- src/presentation/screens/edit-event/index.tsx
+++ src/presentation/screens/edit-event/index.tsx
@@ -1,50 +1,42 @@
-import {AppHeader} from "@/presentation/components/AppHeader.tsx";
-import {Button} from "@/presentation/components/ui/button.tsx";
 import {CalendarClock, Camera, Dot, Pencil, UsersRound} from "lucide-react";
-import {EventEndDateModal} from "@/presentation/screens/edit-event/components/EventEndDateModal.tsx";
-import {useEditEvent} from "@/presentation/screens/edit-event/useEditEvent.ts";
+
 import {formatDate} from "@/core/utils/formatDate.ts";
 import {formatTime} from "@/core/utils/formatTime.ts";
+
+import {Button} from "@/presentation/components/ui/button.tsx";
+import {AppHeader} from "@/presentation/components/AppHeader.tsx";
+import {EventEndDateModal} from "@/presentation/screens/edit-event/components/EventEndDateModal.tsx";
 import {EventNameModal} from "@/presentation/screens/edit-event/components/EventNameModal.tsx";
 import {NumberOfPhotosModal} from "@/presentation/screens/edit-event/components/NumberOfPhotosModal.tsx";
 import {NumberOfGuestsModal} from "@/presentation/screens/edit-event/components/NumberOfGuestsModal.tsx";
+import {useEventForm} from "@/presentation/screens/edit-event/useEventForm.ts";
 
 
 export function EditEvent() {
-
     const {
-        endDateModalOpen,
-        eventNameModalOpen,
-        numberOfPhotosModalOpen,
-        numberOfGuestModalOpen,
         eventName,
         eventEndDate,
         numberOfGuests,
         numberOfPhotos,
-        toggleEventNameModalOpen,
-        toggleEndDateModalOpen,
-        toggleNumberOfPhotosModalOpen,
-        toggleNumberOfGuestsModalOpen,
-        editEventName,
-        selectEventEndDate,
-        selectNumberOfPhotos,
-        selectNumberOfGuests,
-        onGoBack
-    } = useEditEvent()
+        modals,
+        isSubmitting,
+        onSubmitEvent,
+        toggleModal,
+        setField,
+        onGoBack,
+    } = useEventForm()
 
     const Title = () => <div className="flex justify-center items-center gap-2">
         <h2 className="font-black text-xl">
             {eventName}
         </h2>
-        <Button onClick={toggleEventNameModalOpen} size="sm" variant="secondary" className="p-0">
+        <Button onClick={() => toggleModal("eventName")} size="sm" variant="secondary" className="p-0">
             <Pencil size={16}/>
         </Button>
     </div>
 
-    const onClickContinue = () => {
-        const event = {eventName, eventEndDate, numberOfGuests, numberOfPhotos}
-
-        console.log("Event:", event)
+    const onClickContinue = async () => {
+        await onSubmitEvent()
     }
 
     return (
@@ -64,7 +56,7 @@ export function EditEvent() {
 
                     <div className="space-y-4 grid content-end">
                         <div className="grid grid-cols-[auto_1fr] items-center gap-x-2"
-                             onClick={toggleEndDateModalOpen}>
+                             onClick={() => toggleModal("endDate")}>
                             <CalendarClock className="row-span-2"/>
                             <p className="text-xs text-secondary-foreground/40">End</p>
                             <div className="flex items-center">
@@ -73,19 +65,19 @@ export function EditEvent() {
                         </div>
 
                         <div className="grid grid-cols-[auto_1fr] items-center gap-x-2"
-                             onClick={toggleNumberOfGuestsModalOpen}>
+                             onClick={() => toggleModal("numberOfGuests")}>
                             <UsersRound className="row-span-2"/>
                             <p className="text-xs text-secondary-foreground/40">Number of Guests</p>
                             <p>{numberOfGuests} People</p>
                         </div>
 
                         <div className="grid grid-cols-[auto_1fr] items-center gap-x-2"
-                             onClick={toggleNumberOfPhotosModalOpen}>
+                             onClick={() => toggleModal("numberOfPhotos")}>
                             <Camera className="row-span-2"/>
                             <p className="text-xs text-secondary-foreground/40">Number of photos per guest</p>
                             <p>{numberOfPhotos} Photos</p>
                         </div>
-                        <Button className="w-full" onClick={onClickContinue}>
+                        <Button className="w-full" onClick={onClickContinue} disabled={isSubmitting}>
                             CONTINUE
                         </Button>
                     </div>
@@ -93,26 +85,26 @@ export function EditEvent() {
             </div>
             <EventNameModal
                 eventName={eventName}
-                onEditEventName={editEventName}
-                isOpen={eventNameModalOpen}
-                onClose={toggleEventNameModalOpen}
+                onEditEventName={(eventName) => setField("eventName", eventName)}
+                isOpen={modals.eventName}
+                onClose={() => toggleModal("eventName")}
             />
             <EventEndDateModal
-                isOpen={endDateModalOpen}
-                onClose={toggleEndDateModalOpen}
-                onSelectDate={selectEventEndDate}
+                isOpen={modals.endDate}
+                onClose={() => toggleModal("endDate")}
+                onSelectDate={date => setField("eventEndDate", date)}
             />
             <NumberOfPhotosModal
-                isOpen={numberOfPhotosModalOpen}
+                isOpen={modals.numberOfPhotos}
                 numberOfPhotos={numberOfPhotos}
-                onClose={toggleNumberOfPhotosModalOpen}
-                onSelectNumberOfPhotos={selectNumberOfPhotos}
+                onClose={() => toggleModal("numberOfPhotos")}
+                onSelectNumberOfPhotos={numberOfPhotos => setField("numberOfPhotos", numberOfPhotos)}
             />
             <NumberOfGuestsModal
-                isOpen={numberOfGuestModalOpen}
+                isOpen={modals.numberOfGuests}
                 numberOfGuest={numberOfGuests}
-                onClose={toggleNumberOfGuestsModalOpen}
-                onSelectNumberOfGuests={selectNumberOfGuests}
+                onClose={() => toggleModal("numberOfGuests")}
+                onSelectNumberOfGuests={numberOfGuest => setField("numberOfGuests", numberOfGuest)}
             />
         </>
     )
