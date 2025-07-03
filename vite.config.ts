5c7d5913763c21705c4b1187449f02276dc3b2c7
aboli <duniandewon@gmail.com>
2025-07-03 20:37:57 +0700

    Use https for local dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
 import path from "path"
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import * as fs from "node:fs";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react(), tailwindcss()],
+    server: {
+        https: {
+            key: fs.readFileSync('./certs/localhost-key.pem'),
+            cert: fs.readFileSync('./certs/localhost.pem'),
+        },
+        host: true,
     },
-  },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 })
